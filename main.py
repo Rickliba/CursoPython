ec776e50f33ff85fc1634428eafaa9c4366d4452
Henrique Libarino <henrique.libarino.ext@dasa.com.br>
2024-01-18 17:49:57 -0300

    Exc 18 e 19

---
+++ main.py
@@ -0,0 +1,19 @@
+import math, random
+
+# EXERCICIO 18
+
+# angulo = float(input('Digite seu angulo ai arrombado: '))
+# print(f'Seno: {sin(radians(angulo)):.2f}, Cosseno: {cos(radians(angulo)):.2f}, Tangente: {tan(radians(angulo)):.2f}')
+
+# EXERCICIO 19
+
+# aluno1 = str(input('Nome do primeiro aluno: '))
+# aluno2 = str(input('Nome do segundo aluno: '))
+# aluno3 = str(input('Nome do terceiro aluno: '))
+# aluno4 = str(input('Nome do quarto aluno: '))
+
+# lista = [aluno1, aluno2, aluno3, aluno4]
+# escolha = random.choice(lista)
+# print(f'O aluno escolhido foi {escolha}')
+
+
