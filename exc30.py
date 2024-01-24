e8ed99153b9d79fe2b1f478ad4008d9ddebbed09
Henrique Libarino <henrique.libarino.ext@dasa.com.br>
2024-01-24 14:13:38 -0300

    EXC 30 feito

---
+++ exc30.py
@@ -0,0 +1,9 @@
+import math, random
+
+num = int(input('Digite um numero inteiro e maior que 0: '))
+
+if num % 2 == 0: 
+    print('Boa, seu numero é par')
+
+else: 
+    print('Seu numero é ímpar')
