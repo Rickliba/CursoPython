5e76c9bfffd85a44aa74b9d99636adfa664f4ee6
Henrique Libarino <henrique.libarino.ext@dasa.com.br>
2024-01-24 12:11:00 -0300

    Exc 25 resolvido

---
+++ exc25.py
@@ -0,0 +1,8 @@
+import random, math
+
+#Exc 25 - Procure Silva no nome que for digitado
+
+nome = str(input('Digite seu nome: ')).strip()
+
+print('Seu nome tem silva? {}'.format('silva' in nome.lower()))
+
